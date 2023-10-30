e1ca9dfc34c1b55e659d39b39d174f335008c3ec
sulphitic786 <wqasimg@gmail.com>
2023-10-30 12:37:25 +0500

    chnage tail iwnd config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors:{
-        'primary':'#1E559B',
-      },
-    
+             'primary':'#1E559B',
+       },
     },
   },
   plugins: [],
